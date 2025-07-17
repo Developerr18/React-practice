6ad3bac69abc6be3cd69c8744d6402a95525dc16
Developerr18 <dineshnikum1@gmail.com>
2025-07-17 18:22:58 +0530

    commit //

--- best-practices/src/App.jsx
+++ best-practices/src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
-import SignupForm from "./controlled-form/ChatgptForm";
+// import SignupForm from "./controlled-form/ChatgptForm";
 import ControlledForm from "./controlled-form/ClaudeForm";
-import Form from "./controlled-form/Form";
-import Counter from "./counter/Counter";
+// import Form from "./controlled-form/Form";
+// import Counter from "./counter/Counter";
 
 export default function App() {
     return <ControlledForm />;
