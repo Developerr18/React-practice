53a9cf82b73c23ee5827737c83e22cf6f1975eaf
Developerr18 <dineshnikum1@gmail.com>
2025-07-17 18:22:13 +0530

    add file

--- best-practices/src/App.jsx
+++ best-practices/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import SignupForm from "./controlled-form/ChatgptForm";
 import ControlledForm from "./controlled-form/ClaudeForm";
 import Form from "./controlled-form/Form";
-// import Counter from "./counter/Counter";
+import Counter from "./counter/Counter";
 
 export default function App() {
     return <ControlledForm />;
