cf3e40e8a2e0f6b464d530e407d7823b0ba7add0
Developerr18 <dineshnikum1@gmail.com>
2025-07-18 15:24:17 +0530

    modified

--- best-practices/src/App.jsx
+++ best-practices/src/App.jsx
@@ -2,5 +2,5 @@ import "./App.css";
 import FormValidation from "./controlled-form/MyClaudeForm";
 
 export default function App() {
-    return <FormValidation type="form" />;
+    return <FormValidation />;
 }
