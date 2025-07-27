9a33ba875e75a4ea7447541db0c881d9c85b49aa
Developerr18 <dineshnikum1@gmail.com>
2025-07-27 23:44:06 +0530

    update

--- best-practices/src/useReducer/ReducerTodo.jsx
+++ best-practices/src/useReducer/ReducerTodo.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 
 const initialTodos = [
   {
@@ -18,23 +18,66 @@ const initialTodos = [
   },
 ];
 
-export default function MyTodo() {
-  const [todos, setTodos] = useState(initialTodos);
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case "AddedTodo":
+      return [
+        ...todos,
+        { id: todos.length + 1, title: action.title, done: false },
+      ];
+    case "Changed_Todo":
+      return todos.map((todo) => {
+        if (todo.id === action.updatedTodo.id) {
+          return action.updatedTodo;
+        } else {
+          return todo;
+        }
+      });
+    case "Delete_Todo":
+      return todos.filter((todo) => todo.id !== action.todoId);
+    default:
+      throw new Error("Unknown action: " + action.type);
+  }
+}
 
+export default function MyTodo() {
+  const [todos, dispatchTodos] = useReducer(todoReducer, initialTodos);
+  // const [todos, setTodos] = useState(initialTodos);
+
+  function handleAddTodo(newTodo) {
+    dispatchTodos({
+      type: "AddedTodo",
+      title: newTodo,
+    });
+  }
+
+  function handleChangeTodo(updatedTodo) {
+    dispatchTodos({
+      type: "Changed_Todo",
+      updatedTodo,
+    });
+  }
+
+  function handleDeleteTodo(todoId) {
+    dispatchTodos({
+      type: "Delete_Todo",
+      todoId,
+    });
+  }
+
+  /*
   const handleAddTodo = (newTodo) => {
     if (!newTodo.trim()) return;
     setTodos([...todos, { id: todos.length + 1, title: newTodo, done: false }]);
   };
-
   const handleChangeTodo = (updatedTodo) => {
     setTodos(
       todos.map((todo) => (todo.id === updatedTodo.id ? updatedTodo : todo))
     );
   };
-
-  const handleDelete = (id) => {
+  const handleDeleteTodo = (id) => {
     setTodos(todos.filter((todo) => todo.id !== id));
-  };
+  };*/
 
   return (
     <div>
@@ -45,7 +88,7 @@ export default function MyTodo() {
             key={todo.id}
             todo={todo}
             onChange={handleChangeTodo}
-            onDelete={handleDelete}
+            onDelete={handleDeleteTodo}
           />
         ))}
       </ul>
@@ -60,6 +103,7 @@ function MyTodoItem({ todo, onChange, onDelete }) {
     <li key={todo.id} className="m-5">
       {isEditing ? (
         <input
+          type="text"
           className="border"
           value={todo.title}
           onChange={(e) => {
@@ -73,6 +117,7 @@ function MyTodoItem({ todo, onChange, onDelete }) {
         <>
           <input
             type="checkbox"
+            className="m-2"
             checked={todo.done}
             onChange={(e) => {
               onChange({
