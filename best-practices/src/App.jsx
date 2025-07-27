f946622b44dbae267b70fe9fc7d2d5c9833960f2
Developerr18 <dineshnikum1@gmail.com>
2025-07-27 20:55:21 +0530

    move from setting state to reducer

--- best-practices/src/App.jsx
+++ best-practices/src/App.jsx
@@ -1,11 +1,6 @@
 import "./App.css";
-// import Accordian from "./components/Accordian";
-// import CustomCounter from "./custom-hook/Custom";
-import ExampleApp from "./ThinkingInReact";
-// import MyGame from "./Tic-Tac-Toe/MyGame";
-// import FormValidation from "./controlled-form/MyClaudeForm";
-// import TodoApp from "./todo/TodoClaude";
+import ReducerBucketList from "./useReducer/Reducer";
 
 export default function App() {
-    return <ExampleApp />;
+  return <ReducerBucketList />;
 }
---
+++ best-practices/src/useReducer/Reducer.jsx
@@ -0,0 +1,104 @@
+import { useReducer, useState } from "react";
+
+const initialArtworkList = [
+  { id: 0, title: "Big Bellies", seen: false },
+  { id: 1, title: "Lunar Landscape", seen: false },
+  { id: 2, title: "Terracotta Army", seen: true },
+];
+
+function artReducer(artList, action) {
+  switch (action.type) {
+    case "TOGGLE_SEEN":
+      return artList.map((artwork) => {
+        if (artwork.id === action.artId) {
+          return { ...artwork, seen: action.seen };
+        } else {
+          return artwork;
+        }
+      });
+  }
+}
+
+export default function ReducerBucketList() {
+  const [myList, dispatchMyList] = useReducer(artReducer, initialArtworkList);
+  const [yourList, dispatchYourList] = useReducer(
+    artReducer,
+    initialArtworkList
+  );
+
+  function handleToggleMyList(artworkId, nextSeen) {
+    dispatchMyList({
+      type: "TOGGLE_SEEN",
+      artId: artworkId,
+      seen: nextSeen,
+    });
+  }
+
+  function handleToggleYourList(artworkId, nextSeen) {
+    dispatchYourList({
+      type: "TOGGLE_SEEN",
+      artId: artworkId,
+      seen: nextSeen,
+    });
+  }
+
+  /*
+  const [myList, setMyList] = useState(initialList);
+  const [yourList, setYourList] = useState(initialList);
+  
+  function handleToggleMyList(artworkId, nextSeen) {
+    setMyList(
+      myList.map((artwork) => {
+        if (artwork.id === artworkId) {
+          return { ...artwork, seen: nextSeen };
+        } else {
+          return artwork;
+        }
+      })
+    );
+  }
+  function handleToggleYourList(artworkId, nextSeen) {
+    setYourList(
+      yourList.map((artwork) => {
+        if (artwork.id === artworkId) {
+          return { ...artwork, seen: nextSeen };
+        } else {
+          return artwork;
+        }
+      })
+    );
+  }*/
+
+  return (
+    <>
+      <h1 className="text-2xl">Art Bucket List With Reducer</h1>
+      <hr />
+      <h2>My list of art to see:</h2>
+      <ItemList artworks={myList} onToggle={handleToggleMyList} />
+      <hr />
+      <h2>Your list of art to see:</h2>
+      <ItemList artworks={yourList} onToggle={handleToggleYourList} />
+    </>
+  );
+}
+
+function ItemList({ artworks, onToggle }) {
+  return (
+    <ul>
+      {artworks.map((artwork) => (
+        <li key={artwork.id}>
+          <label>
+            <input
+              type="checkbox"
+              checked={artwork.seen}
+              onChange={(e) => {
+                onToggle(artwork.id, e.target.checked);
+              }}
+            />
+            {artwork.title}
+          </label>
+        </li>
+      ))}
+    </ul>
+  );
+}
